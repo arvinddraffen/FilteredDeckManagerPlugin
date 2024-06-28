0f32b592572dd5b3a877f273d5f443c042f947b6
Arvind Draffen <arvinddraffen@gmail.com>
2024-06-27 19:07:02 -0500

    Update function naming of menu action.

--- FilteredDeckManager/__init__.py
+++ FilteredDeckManager/__init__.py
@@ -5,19 +5,15 @@ from aqt.utils import showInfo, qconnect
 # import all of the Qt GUI library
 from aqt.qt import *
 
-# We're going to add a menu item below. First we want to create a function to
-# be called when the menu item is activated.
 
 from .UI import MainUI
 dialogUI = MainUI.MainUI(mw)
 
-def testFunction() -> None:
+def launch() -> None:
     dialogUI.InitializeData()
     dialogUI.exec()
 
-# create a new menu item, "test"
 action = QAction("Filtered Deck Manager", mw)
-# set it to call testFunction when it's clicked
-qconnect(action.triggered, testFunction)
+qconnect(action.triggered, launch)
 # and add it to the tools menu
 mw.form.menuTools.addAction(action)
